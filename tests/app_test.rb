956ba88c6bfc2b164df0a9d6d59a3f53e93128a8
Murilo Lucas <muhlucas@outlook.com>
2017-01-28 15:16:47 -0200

    implementação da busca com o google

--- tests/app_test.rb
+++ tests/app_test.rb
@@ -2,6 +2,8 @@
 require_relative '../app'
 require 'minitest/autorun'
 require 'rack/test'
+require 'sinatra'
+require 'httparty'
 
 class AppTest < Minitest::Test
   include Rack::Test::Methods
@@ -13,5 +15,13 @@ class AppTest < Minitest::Test
   def test_home_busca_de_endereco
     get '/'
     assert_match /Busca de Endereço/, last_response.body
+  end
+
+  def test_requisicao
+      endereco = "Rua casa do ator, sp"
+      endereco_escaped = URI.escape(endereco)
+      url = "http://maps.google.com/maps/api/geocode/json?address=#{endereco_escaped}"
+      response = HTTParty.get(url)
+      assert(200, response.code)
   end
 end
