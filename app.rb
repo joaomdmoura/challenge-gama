d6ee9ea88d686c2509c03e3a0ee051d500e540f4
Joao M. D. Moura <joaomdmoura@gmail.com>
2017-01-28 16:06:32 -0200

    quick-fix

--- app.rb
+++ app.rb
@@ -4,10 +4,12 @@ require 'sinatra'
 require 'httparty'
 
 get '/' do
-  escaped  = URI.escape(params['url'])
-  url      = "http://maps.google.com/maps/api/geocode/json?address=#{escaped}"
-  response = HTTParty.get(url)
-  parsed   = JSON.parse(response.body)['results']
+  parsed = if params['url']
+    escaped  = URI.escape(params['url'])
+    url      = "http://maps.google.com/maps/api/geocode/json?address=#{escaped}"
+    response = HTTParty.get(url)
+    JSON.parse(response.body)['results']
+  end
 
   erb :index, :locals => {result: parsed}
 end
