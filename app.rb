6105036834cfe78c13611fcf59ef27d677c497cd
Raoni1984 <raonico@gmail.com>
2017-03-25 15:26:21 -0300

    Adicionada integracao com Google Maps

--- app.rb
+++ app.rb
@@ -4,5 +4,19 @@ require 'sinatra'
 require 'httparty'
 
 get '/' do
-  erb :index, :locals => {result: nil}
+
+  endereco = params["url"]
+
+  if endereco
+    
+    endereco_escape = URI.escape(endereco)
+    url = "http://maps.google.com/maps/api/geocode/json?address=#{endereco_escape}"
+    response = HTTParty.get(url)
+    parsed = JSON.parse(response.body)['results']
+
+    erb :index, :locals => {result: parsed}
+  else
+    erb :index, :locals => {result: nil}
+  end
+
 end
--- tests/app_test.rb
+++ tests/app_test.rb
@@ -14,4 +14,9 @@ class AppTest < Minitest::Test
     get '/'
     assert_match /Busca de Endereço/, last_response.body
   end
+
+  def test_busca_de_endereco
+    get '/?url=avenida+paulisa_2002'
+    assert_match /Avenida Paulista/, last_response.body
+  end
 end
