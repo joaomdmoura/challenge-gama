c5c4156b0a419fd79e17573b9b613e9ddc8c5687
Caio Le Petit <caio.lepetit@gmail.com>
2017-03-25 15:03:10 -0300

    Funcionando o teste de endereço

--- app.rb
+++ app.rb
@@ -5,8 +5,12 @@ require 'httparty'
 
 get '/' do
 
+  require 'httparty'
   endereco = params['url']
   escaped  = URI.escape(endereco)
+  url      = "http://maps.google.com/maps/api/geocode/json?address=#{escaped}"
+  response = HTTParty.get(url)
+  parsed   = JSON.parse(response.body)['results']
 
-  erb :index, :locals => {result: escaped}
+  erb :index, :locals => {result: parsed}
 end
