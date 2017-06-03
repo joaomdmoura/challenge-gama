667ce108a8f4efbaa56ae00bc85c5df1efa3eb7b
Luborges <lucas.s.m.b@hotmail.com>
2017-06-03 15:43:04 -0300

    Criação de testes de do aplicativo

--- app.rb
+++ app.rb
@@ -4,5 +4,16 @@ require 'sinatra'
 require 'httparty'
 
 get '/' do
-  erb :index, :locals => {result: nil}
+	url = params[:url]
+	if url.nil?
+		result=''
+ 	elsif url == '' then
+ 		result = "Endereço Invalido" 
+ 	else
+ 		url = url.gsub(' ', '+')
+ 		url = URI.escape(url)
+ 		response = HTTParty.get("http://maps.google.com/maps/api/geocode/json?address="+url)
+ 		result = JSON.parse(response.body)['results']
+ 	end
+  	erb :index, :locals => {result: result}
 end
