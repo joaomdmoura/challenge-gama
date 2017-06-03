a8289bb555c841cdb7fae5ea8d4ec1170ef9b4c4
Antonio Castanheira <antonio.augusto.castanheira@gmail.com>
2017-06-03 15:47:16 -0300

    New feature geolocation

--- app.rb
+++ app.rb
@@ -4,5 +4,20 @@ require 'sinatra'
 require 'httparty'
 
 get '/' do
-  erb :index, :locals => {result: nil}
+  url = params[:url]
+  result = case url
+
+  when nil
+  	""
+  when ""
+  	"endereco invalido"
+  else
+  result = "endereco invalido" if url ==""
+  endereco = params[:url].gsub(' ', '+')
+  escaped  = URI.escape(endereco)
+  url      = "http://maps.google.com/maps/api/geocode/json?address=#{escaped}"
+  response = HTTParty.get(url)
+  result   = JSON.parse(response.body)['results']
+end
+  erb :index, :locals => {result: result}
 end
