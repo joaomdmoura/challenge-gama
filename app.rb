8950a78908474a9b145cf677ca6211d4c1dff380
atomic-avocado <victor@automobi.com.br>
2017-08-05 15:04:33 -0300

    formatting texts

--- app.rb
+++ app.rb
@@ -4,14 +4,18 @@ require 'sinatra'
 require 'httparty'
 
 get '/' do
-	if params[:url] && params[:url] != '' then 
-		escaped = URI.escape(params[:url])
-		url      = "http://maps.google.com/maps/api/geocode/json?address=#{escaped}"
-		response = HTTParty.get(url)
-		parsed   = JSON.parse(response.body)['results']
-		erb :index, :locals => {result: parsed}
+	if params[:url] then 
+		if params[:url] == '' then 
+			erb :index, :locals => {result: nil, error: 'Endereço Inválido'}
+		else
+			escaped = URI.escape(params[:url])
+			url      = "http://maps.google.com/maps/api/geocode/json?address=#{escaped}"
+			response = HTTParty.get(url)
+			parsed   = JSON.parse(response.body)['results']
+			erb :index, :locals => {result: parsed.first, error: nil}
+		end
 	else
-		erb :index, :locals => {result: nil}	
+		erb :index, :locals => {result: nil, error: nil}	
 	end
 	
 end
