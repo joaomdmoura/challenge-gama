3a8636f070181dd79c4a45bb61f04ac17a67b1c9
Henrique Carrasco <henrique.carrasco.1@gmail.com>
2017-08-05 18:55:06 +0000

    maps integration

--- app.rb
+++ app.rb
@@ -4,5 +4,15 @@ require 'sinatra'
 require 'httparty'
 
 get '/' do
-  erb :index, :locals => {result: nil}
+  endereco = params['url']
+  if endereco || endereco != '' then
+    escaped = URI.escape(endereco)
+    url = "http://maps.google.com/maps/api/geocode/json?address=#{escaped}"
+    response = HTTParty.get(url)
+    parsed   = JSON.parse(response.body)['results']
+  else
+    parsed = "Endereço Inválido"
+  end
+  
+  erb :index, :locals => {result: parsed}
 end
--- tests/app_test.rb
+++ tests/app_test.rb
@@ -3,6 +3,7 @@ require_relative '../app'
 require 'minitest/autorun'
 require 'rack/test'
 
+
 class AppTest < Minitest::Test
   include Rack::Test::Methods
 
@@ -14,4 +15,17 @@ class AppTest < Minitest::Test
     get '/'
     assert_match /Busca de Endereço/, last_response.body
   end
+  
+  def test_home_valid_address_search
+    endereco = 'Rua Casa do Ator 275'
+    escaped  = URI.escape(endereco)
+    get "/?url=#{escaped}"
+    assert_match /Rua Casa do Ator 275/, last_response.body
+  end
+  
+  def test_home_invalid_search
+    get '/url='
+    assert_match /endereco invalido/, last_response.body
+  end
+  
 end
